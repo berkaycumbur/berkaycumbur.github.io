f8c6867b3d7d76d0cb518443645d629d1333e86f
Berkay Çumbur <cumburberkay@gmail.com>
2022-07-02 00:07:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const container = document.getElementById("container");
+const SQUARES = 1400;
+
+function generateRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+for (let i = 0; i < SQUARES; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", () => setColor(square));
+
+  square.addEventListener("mouseout", () => removeColor(square));
+
+  container.appendChild(square);
+}
+
+function setColor(element) {
+  const color = generateRandomColor();
+  element.style.background = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+  element.style.background = "#1d1d1d";
+  element.style.boxShadow = "0 0 2px #000";
+}
